99989e42af2b23d8c666d7b7593221c70a1c3c26
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2024-07-18 20:17:11 +0900

    DEBUGモードでRQT_PLOTを利用可能にした．

--- robot_visualizer/launch/robot_visualizer.launch.py
+++ robot_visualizer/launch/robot_visualizer.launch.py
@@ -1,8 +1,10 @@
 # Visualizer launch file
 import os
 from time import sleep
+import yaml
 
 import launch
+from launch.actions import TimerAction
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -10,14 +12,19 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
   launch_description = launch.LaunchDescription()
 
+  debug_mode_yaml_path = os.path.join(get_package_share_directory("robot_bringup"), "config", "param_debug_mode.yaml")
+  with open(debug_mode_yaml_path, "r") as f:
+    debug_mode_yaml = yaml.safe_load(f)["/**"]["ros__parameters"]["setting_debug_mode"]
+
 # Rviz2のlaunch
-  rviz_launch = launch.actions.IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([
-      os.path.join(get_package_share_directory("robot_visualizer"), "launch"),
-      "/rviz.launch.py"
-    ])
-  )
-  launch_description.add_action(rviz_launch)
+  if debug_mode_yaml["using_rviz"] == True:
+    rviz_launch = launch.actions.IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        os.path.join(get_package_share_directory("robot_visualizer"), "launch"),
+        "/rviz.launch.py"
+      ])
+    )
+    launch_description.add_action(rviz_launch)
 
 # Loggerとの違いは、リアルタイムに表示をするだけ　｜　記録を取る
 
@@ -25,7 +32,22 @@ def generate_launch_description():
 # ros関連を詳しく出してくれるtopを実行するのも良さげ？
   # これ：https://github.com/iwatake2222/rotop
     # TODO: 動作確認と、pythonソースから実行できるか確認（コマンドラインでも、pythonでも）
-  
+
+  # RQT Plotのlaunch
+  if debug_mode_yaml["using_rqt"] == True:
+    rqt_plot_launch = Node(
+      package = "rqt_plot",
+      executable = "rqt_plot",
+      output = "screen",
+      arguments = [
+        "/joint_states/position[9]",  # DEBUG: ここをDEBUG用のPublish Topicを指定すれば良い．んで，これをparameterから指定するようにすれば良い．
+      ]
+    )
+    rqt_plot_launch_delay = TimerAction(
+      period = 1.0, 
+      actions = [rqt_plot_launch]
+    )
+    launch_description.add_action(rqt_plot_launch_delay)
 
   # sleep(4)
   return launch_description

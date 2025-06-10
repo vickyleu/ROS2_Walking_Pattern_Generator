690cba91c862eaa54f4d0480c338a03fb38abd48
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2025-06-10 17:15:42 +0900

    ikの間違いを修正

--- draft_programs/rust/fk_ik/src/main.rs
+++ draft_programs/rust/fk_ik/src/main.rs
@@ -19,44 +19,61 @@ fn main() {
   println!("vp_c_re: {}", &vp_c_re);
 
 // IK
-  let el1: i32 = ((&vp_c_re[1] - &vp_c_rwl[1]) * 1000.0) as i32;
-  let el2: f64 = el1 as f64 / 1000.;  // 丸め誤差だかが原因でacos()がnanになるので、i32に変換してからfloatに戻す
-  theta_rwl = (el2 / &vp_rwl_rwp[1]).acos();
+  //let el1: i32 = ((&vp_c_re[1] - &vp_c_rwl[1]) * 1000.0) as i32;
+  //let el2: f64 = el1 as f64 / 1000.;  // 丸め誤差だかが原因でacos()がnanになるので、i32に変換してからfloatに戻す
+  theta_rwl = std::f64::consts::PI / 2. - &vp_c_re[2].abs().atan2(&vp_c_re[1] - (&vp_c_rwl[1] + &vp_rwl_rwp[1]));
   println!("theta_rwl: {}", &theta_rwl * 180. / std::f64::consts::PI);
 
-  let rm_rwl: Array2<f64> = array![
-    [1., 0., 0.,],
-    [0., theta_rwl.cos(), -theta_rwl.sin()],
-    [0., theta_rwl.sin(), theta_rwl.cos()]
+  // let rm_rwl: Array2<f64> = array![
+  //   [1., 0., 0.,],
+  //   [0., theta_rwl.cos(), -theta_rwl.sin()],
+  //   [0., theta_rwl.sin(), theta_rwl.cos()]
+  // ];
+  let T_wr: Array2<f64> = array![
+    [1., 0.             , 0.              , -0.091],
+    [0., theta_rwl.cos(), -theta_rwl.sin(), -0.075],
+    [0., theta_rwl.sin(),  theta_rwl.cos(),  0.],
+    [0., 0.             , 0.              ,  1.]
   ];
-  let vp_c_rwp: Array1<f64> = &vp_c_rwl + &rm_rwl.dot(&vp_rwl_rwp);
-  //println!("vp_c_rwp: {}", &vp_c_rwp);
-  let vr: Array1<f64> = &vp_c_re - &vp_c_rwp;
-  //println!("vr: {}", &vr);
-  let a: f64 = ((&vr[0].powf(2.) + &vr[1].powf(2.)).sqrt() + &vr[2].powf(2.)).sqrt();
-  //println!("a: {}", &a);  // ここまでOK
-
-  let _el3 = (-&a.powf(2.) + &vp_rkp_re[2].abs().powf(2.) + &vp_rwp_rkp[2].abs().powf(2.)) / (2. * &vp_rkp_re[2].abs() * &vp_rwp_rkp[2].abs());
-  //println!("el3: {}", &el3);
-  let _el4 = -&a.powf(2.) + &vp_rkp_re[2].abs().powf(2.) + &vp_rwp_rkp[2].abs().powf(2.);
-  //println!("el4: {} ({} {} {})", &el4, -a.powf(2.), &vp_rkp_re[2].abs().powf(2.), &vp_rwp_rkp[2].abs().powf(2.));
-  theta_rkp = ((-&a.powf(2.) + &vp_rkp_re[2].powf(2.) + &vp_rwp_rkp[2].powf(2.)) / (2. * &vp_rkp_re[2].abs() * &vp_rwp_rkp[2].abs())).acos();
-
-  theta_rwp = ((-&vp_rkp_re[2].powf(2.) + &a.powf(2.) + &vp_rwp_rkp[2].powf(2.)) / (2. * &a.abs() * &vp_rwp_rkp[2].abs())).acos();
+  let vv: Array2<f64> = array![[vp_rwl_rwp[0]], [vp_rwl_rwp[1]], [vp_rwl_rwp[2]], [1.]];
+  let vp_c_rwp: Array2<f64> = T_wr.dot(&vv);
+  println!("vp_c_rwp: {}", &vp_c_rwp.t());
+  let vr: Array1<f64> = &vp_c_re - array![vp_c_rwp[[0,0]], vp_c_rwp[[1,0]], vp_c_rwp[[2,0]]];
+  println!("vr: {}", &vr);
+  let a: f64 = vp_rwp_rkp[2].abs();
+  let b: f64 = vp_rkp_re[2].abs();
+  let c: f64 = (&vr[0].powf(2.) + &vr[1].powf(2.) + &vr[2].powf(2.)).sqrt();
+  println!("c: {}",&c);
+  theta_rkp = -(((&a.powf(2.) + &b.powf(2.) - &c.powf(2.))/(2.*&a*&b)).acos()) + std::f64::consts::PI;
+  let foo: f64 = vr[0].atan2((&vr[1].powf(2.) + &vr[2].powf(2.)).sqrt());  // 目標X軸座標と重心X座標を結ぶ直線の傾きがZ軸となす角度
+  let bar: f64 = ((&b*(std::f64::consts::PI - &theta_rkp).sin())/(&c)).asin();  // 腰Yaw軸から足先までからなる三角形での腰Yaw軸にあたる内角の角度
+  println!("foo: {}", &foo * 180. / std::f64::consts::PI);
+  println!("bar: {}", &bar * 180. / std::f64::consts::PI);
+  theta_rwp = -1. * (foo + bar);  // 回転方向の修正
   println!("theta_rwp: {}", &theta_rwp * 180. / std::f64::consts::PI);
-
   println!("theta_rkp: {}", &theta_rkp * 180. / std::f64::consts::PI);
 
 // FK
-  let rm_rwp: Array2<f64> = array![
-    [-theta_rwp.cos(), 0., -theta_rwp.sin()],
-    [0., 1., 0.],
-    [theta_rwp.sin(), 0., -theta_rwp.cos()]
+  // let T_wr: Array2<f64> = array![
+  //   [1., 0.             , 0.              , -0.091],
+  //   [0., theta_rwl.cos(), -theta_rwl.sin(), -0.075],
+  //   [0., theta_rwl.sin(),  theta_rwl.cos(),  0.],
+  //   [0., 0.             , 0.              ,  1.]
+  // ];
+  let T_wp: Array2<f64> = array![
+    [ theta_rwp.cos(), 0., theta_rwp.sin(),  0.091],
+    [ 0.             , 1., 0.             , -0.055],
+    [-theta_rwp.sin(), 0., theta_rwp.cos(),  0.],
+    [ 0.             , 0., 0.             ,  1.]
   ];
-  let rm_rkp: Array2<f64> = array![
-    [theta_rkp.cos(), 0., theta_rkp.sin()],
-    [0., 1., 0.],
-    [-theta_rkp.sin(), 0., theta_rkp.cos()]
+  let T_kp: Array2<f64> = array![
+    [ theta_rkp.cos(), 0., theta_rkp.sin(),  0.],
+    [ 0.             , 1., 0.             ,  0.],
+    [-theta_rkp.sin(), 0., theta_rkp.cos(), -0.16],
+    [ 0.             , 0., 0.             ,  1.]
   ];
+  let T_e: Array2<f64> = array![[0.], [0.], [-0.173], [1.]];
 
+  let fk_result: Array2<f64> = T_wr.dot(&T_wp.dot(&T_kp.dot(&T_e)));
+  println!("fk_result: {}", fk_result.t());
 }

5599b334cb343c2bc88de4cb5b485d8b15b72768
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2025-06-17 13:37:57 +0900

    IKクラスのIKの部分以外を書いた。buildはまだ。

---
+++ rdc_kinematics/include/rdc_kinematics/InverseKinematics.hpp
@@ -0,0 +1,34 @@
+#ifndef RDC_INVERSE_KINEMATICS_HPP
+#define RDC_INVERSE_KINEMATICS_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "robot_manager/control_plugin_bases/PluginBase_InverseKinematics.hpp"
+
+#include "Eigen/Dense"
+
+namespace rdc_kinematics
+{
+  class RDC_InverseKinematics : public control_plugin_base::InverseKinematics {
+    public:
+      RDC_InverseKinematics(){}
+      ~RDC_InverseKinematics(){}
+
+      void inverse_kinematics(
+        const std::shared_ptr<control_plugin_base::LegStates_ToIK> leg_states_ptr,
+        std::array<double, 3>& joint_ang_ptr
+      ) override;
+
+      Eigen::Vector3d Array2Vector(std::array<double, 3> array);  // std::array型をEigen::Vector3d型に変換（３次元）
+      Eigen::Matrix3d Array2Matrix(std::array<double, 9> array);  // std::array型をEigen::Matrix3d型に変換（3*3行列）
+
+    private:
+      Eigen::Matrix3d Rx(double rad = 0);
+      Eigen::Matrix3d Ry(double rad = 0);
+
+      // double sign(double arg = 0);  // return 1 or -1 (argが>=0なら1, <0なら-1を返す)
+
+      const float pi_ = 3.141593;  // 四捨五入済み
+  };
+}
+
+#endif
---
+++ rdc_kinematics/src/InverseKinematics.cpp
@@ -0,0 +1,40 @@
+#include "rdc_kinematics/InverseKinematics.hpp"
+
+using namespace Eigen;
+
+namespace rdc_kinematics
+{
+  Vector3d RDC_InverseKinematics::Array2Vector(std::array<double, 3> array) {
+    return {array[0], array[1], array[2]};
+  }
+  Matrix3d RDC_InverseKinematics::Array2Matrix(std::array<double, 9> array) {
+    Matrix3d R;
+    R << array[0], array[1], array[2],
+         array[3], array[4], array[5],
+         array[6], array[7], array[8];
+    return R;
+  }
+
+  Matrix3d RDC_InverseKinematics::Rx(double rad) {
+    Matrix3d R_x;
+    R_x << 1,        0,         0,
+           0, cos(rad), -sin(rad),
+           0, sin(rad),  cos(rad);
+    return(R_x);
+  }
+  Matrix3d RDC_InverseKinematics::Ry(double rad) {
+    Matrix3d R_y;
+    R_y <<  cos(rad), 0, sin(rad),
+                   0, 1,        0,
+           -sin(rad), 0, cos(rad);
+    return(R_y);
+  }
+
+  void RDC_InverseKinematics::inverse_kinematics(
+    const std::shared_ptr<control_plugin_base::LegStates_ToIK> leg_states_ptr,
+    std::array<double, 3>& joint_ang_ptr
+  ) {
+    // IK (RDC's Leg only)
+
+  }
+}
